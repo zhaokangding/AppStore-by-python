571c825e838076eedb8088f7750881f346f56ed8
zhaokangding <89058722+zhaokangding@users.noreply.github.com>
2022-08-16 16:08:02 +0800

    Add files via upload

---
+++ tcpclient.py
@@ -0,0 +1,71 @@
+from socket import * # 引入套接字模块
+serverName = gethostname() # 得到这台主机名，也就是服务器方的IP地址
+serverPort = 12002 # 服务器程序的端口号, 通常这是公认的，这里我写的服务器程序中为12002
+
+def file_recv(clientSocket, filename, filesize):
+    '''
+    接收安装包
+    :param clientSocket: 套接字接口
+    :param filename: 文件名
+    :filesize: 文件大小
+    :return: 接收成功返回 1 否则返回 -1
+    '''
+    # pass
+    try:
+        size = 0
+        with open(filename, "wb") as file:
+            while size < filesize:
+                value = filesize -size
+                if value > 1024:
+                    getdata = clientSocket.recv(1024)
+                else:
+                    getdata = clientSocket.recv(value)
+                file.write(getdata)
+                size = size + 1024
+        return 1
+    except:
+        return -1
+
+
+while True:
+    clientSocket = socket(AF_INET, SOCK_STREAM) # 创建一个套接字，第一个参数指示底层网络使用IPv4地址，第二个参数指示该套接字是SOCK_STREAM类型，这表明它是一个TCP套接字
+    clientSocket.connect((serverName, serverPort)) # 发起客户与服务器之间的TCP连接，第一个参数为服务器地址，第二个参数为服务器端口号
+
+    command = input("Please input msg: ") # 输入从客户端发给服务器的信息
+
+    # command 是用户输入的命令
+
+    # 退出模块
+    if(command == "close"):
+        clientSocket.send(command.encode())  # 发给服务器
+        messig_recv = clientSocket.recv(1024).decode()  # 接收来自服务器的字符
+        if(messig_recv == "88" ):
+            print("成功退出")
+            clientSocket.close()  # 完成一次TCP连接，关闭客户的套接字
+            break
+        else:
+            print("退出异常，请等待或强制退出！")
+            continue
+
+    clientSocket.send(command.encode()) # 发给服务器
+
+
+
+    messig_recv = clientSocket.recv(1024).decode() # 接收来自服务器的字符
+    if(messig_recv == "未定义的命令，请重新输入"):
+        print('From Server: ', messig_recv) # 打印用于可视化
+
+    '''
+        TODO:
+            对不同的命令处理来自服务器端的不同信息
+            ZRW get install: 将安装包以二进制写的方式转到本地并调用其他命令进行软件的下载
+            ZRW uninstall: 卸载安装包
+            find : 处理来自服务器的软件信息
+            change source: 改变软件源
+    '''
+
+
+
+
+
+    clientSocket.close() # 完成一次TCP连接，关闭客户的套接字
---
+++ tcpserver.py
@@ -0,0 +1,145 @@
+from socket import * # 引入套接字模块
+
+install = "ZRW get install"
+uninstall = "ZRW uninstall"
+find = "find"
+change_source = "change source"
+close = "close"
+
+
+
+def handle_command(connectionSocket, command):
+    '''
+    处理命令
+    :param connectionSocket: 套接字接口
+    :param command: 需要处理的命令
+    :return: 处理成功返回1 否则返回 -1
+    '''
+    command_type = command[0:len(command) - command[::-1].find(" ") - 1]
+    # 按照不同的指令类型进行不同测处理
+    if command_type == install:
+        apkname = command[len(command) - command[::-1].find(" ") : -1]
+        handle_install(connectionSocket, apkname)
+        return 1
+    elif command_type == change_source:
+        new_source = command[len(command) - command[::-1].find(" ") : -1]
+        handle_change_source(connectionSocket, new_source)
+        return 1
+    elif command_type == find:
+        info = command[len(command) - command[::-1].find(" ") : -1]
+        handle_find(connectionSocket, info)
+        return 1
+    else:
+        return -1
+
+
+
+def handle_install(connectionSocket, apkname):
+    '''
+    下载命令，从软件源获得软件包并发送给客户端
+    :param connectionSocket: 套接字接口
+    :param apkname: 安装包名字
+    :return: 成功返回 1 否则返回 -1
+    '''
+
+    '''
+        TODO:  基于软件源或者基于开源工具
+    '''
+    pass
+
+
+
+def handle_change_source(connectionSocket, new_source):
+    '''
+    修改软件源
+    :param connectionSocket: 套接字接口
+    :param new_source: 新的软件源
+    :return: 成功返回 1  否则返回-1
+    '''
+    '''
+        TODO: 完成修改软件源
+    '''
+    pass
+
+
+def handle_find(connectonSocket, info):
+    '''
+    搜索软件信息
+    :param connectonSocket: 套接字
+    :param info: 搜索信息
+    :return: 成功返回 1 否则返回 -1
+    '''
+
+    '''
+        TODO: 完成软件信息的搜索，通过套接字返回给客户端
+              有两类搜索，一直是基于分类，一种是基于名字
+    '''
+    pass
+
+
+
+def create_dic(source):
+    '''
+    获取软件源里面软件的信息
+    :param source: 软件源
+    :return: 一个字典，包含软件源的软件的所有信息
+    '''
+    pass
+
+
+
+
+def file_send(connectionSocket, filename, filesize):
+    '''
+    发送安装包
+    :param connectionSocket: 套接字接口
+    :param filename: 文件名
+    :param filesize: 文件大小
+    :return: 发送成功返回 1 否则返回 -1
+    '''
+    # pass
+    try:
+        size = 0
+        with open(filename, "rb") as file:
+            while size < filesize:
+                filedata = file.read(1024)
+                connectionSocket.send(filedata)
+                size = size + 1024
+        return 1
+    except:
+        return -1
+
+
+command_list = [install, uninstall, find, change_source, close] # 可以处理的指令集合
+
+serverPort = 12002 # 设置服务器端口号为12002
+
+serverSocket = socket(AF_INET, SOCK_STREAM) # 创建欢迎套接字，第一个参数指示底层网络使用IPv4地址，第二个参数指示该套接字是SOCK_STREAM类型，这表明它是一个TCP套接字
+serverSocket.bind(('', serverPort)) # 将端口号与欢迎套接字关联起来
+# serverSocket.listen(1) # 等待某个客户的访问
+print('The server is ready to receive\n') 
+while True:
+    serverSocket.listen(1)  # 等待某个客户的访问
+    connectionSocket, addr = serverSocket.accept() # 当客户访问时，调用accept()方法，为这个特定客户创建一个新套接字，由客户专用
+    command = connectionSocket.recv(1024).decode() # 接收来自客户端的字符
+
+    print('From client: ', command) # 打印用于可视化
+    # 处理客户端的命令
+    command_type = command[0:len(command) - command[::-1].find(" ") - 1]
+    if (command_type not in command_list):
+        messig_back = "未定义的命令，请重新输入"
+        connectionSocket.send(messig_back.encode())  # 返回错误信息
+        connectionSocket.close()  # 关闭这个套接字
+    elif command == "close" :
+        messig_back = "88"
+        connectionSocket.send(messig_back.encode())
+        connectionSocket.close()  # 关闭这个套接字
+        continue
+    else:
+        messig_back = "command valid"
+        connectionSocket.send(messig_back.encode())
+        handle_command(connectionSocket, command) #命令处理函数
+
+
+
+
